ef702c2eb0e292d2d807d9681e8dfca7805a404e
Valentin Neher <99484513+valentinneher@users.noreply.github.com>
2024-04-12 00:53:16 +0200

    Add seoMeta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,11 @@ export default defineNuxtConfig({
   },
   ui: {
     icons: ["mdi"],
-  }
+  },
+  seoMeta: {
+      ogImage: 'og-image.png',
+      title: 'Lumora - Never lose a great idea again.',
+      description: 'Lumora organizes all of your thoughts into a searchable knowledge base, with an AI assistant to help connect ideas.',
+      ogDescription: 'Lumora organizes all of your thoughts into a searchable knowledge base, with an AI assistant to help connect ideas.'
+    }
 })
