96c01ad2f8b48ff17b551d70630a764cb265c358
Valentin Neher <99484513+valentinneher@users.noreply.github.com>
2024-04-12 01:16:25 +0200

    Update ogImage url to test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
     icons: ["mdi"],
   },
   seoMeta: {
-      ogImage: 'og-image.png',
+      ogImage: 'https://akm-img-a-in.tosshub.com/businesstoday/images/story/202310/untitled_design_93-sixteen_nine.jpg?size=948:533',
       title: 'Lumora - Never lose a great idea again.',
       description: 'Lumora organizes all of your thoughts into a searchable knowledge base, with an AI assistant to help connect ideas.',
       ogDescription: 'Lumora organizes all of your thoughts into a searchable knowledge base, with an AI assistant to help connect ideas.'
