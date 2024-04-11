57a29deff9cd52c77cf088a075d68227e950096e
ziad <135853660+ziadomalik@users.noreply.github.com>
2024-04-11 21:21:00 +0200

    feat: new text

--- app.vue
+++ app.vue
@@ -5,8 +5,8 @@
         <img src="./assets/images/lumora.svg" class="z-50 mb-10 w-52" />
     
         <UBadge variant="soft" label="COMING SOON" size="lg" class="rounded-full font-bold" />
-        <h1 class="text-3xl font-semibold">Automatic personalized RTT meditations for your patients</h1>
-        <span class="text-lg font-light mt-4">Record your therapy sessions with your clients and automatically create a RTT meditation at the click of a button, highly personalized to your clients needs.</span>
+        <h1 class="text-3xl font-semibold">Never loose a great idea again.</h1>
+        <span class="text-lg font-light mt-4">Record thoughts right when they happen. <br /> Lumora organizes all of your thoughts into a searchable knowledge base, with an AI assistant to help connect ideas.</span>
     
         <form action="https://submit-form.com/sSwsq1yUB"  class="flex gap-2 w-full">
           <UInput name="email" placeholder="Enter your Email" size="xl" class="flex-1" /> 
