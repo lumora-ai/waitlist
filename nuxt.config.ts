f83547e38aa4a45daea0da2957162e75ba2f0b1d
ziad <135853660+ziadomalik@users.noreply.github.com>
2024-04-12 01:48:29 +0200

    feat: remove og image shi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "nuxt-seo-experiments"],
+  modules: ["@nuxt/ui"],
   colorMode: {
     preference: "light"
   },
   ui: {
     icons: ["mdi"],
   },
-  site: {
-    // production URL
-    url: 'https://lumora.ai',
-  },
-  seoMeta: {
-      ogImage: 'https://akm-img-a-in.tosshub.com/businesstoday/images/story/202310/untitled_design_93-sixteen_nine.jpg?size=948:533',
-      title: 'Lumora - Never lose a great idea again.',
-      description: 'Lumora organizes all of your thoughts into a searchable knowledge base, with an AI assistant to help connect ideas.',
-      ogDescription: 'Lumora organizes all of your thoughts into a searchable knowledge base, with an AI assistant to help connect ideas.'
-    }
 })
